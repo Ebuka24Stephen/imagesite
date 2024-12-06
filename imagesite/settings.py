d156478cd1a024c03daabf6a51017ca5fe3220ce
ebuka <sebuka559@gmail.com>
2024-12-06 19:53:28 +0100

    final commit

--- imagesite/settings.py
+++ imagesite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['Ebuka24Stephen.pythonanywhere.com']
 
 
 # Application definition
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')  
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
